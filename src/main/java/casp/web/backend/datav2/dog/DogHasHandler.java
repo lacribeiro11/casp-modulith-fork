5e9f04685708f3eb836c82457355094507183f6d
Luis Ribeiro <lacribeiro11@gmail.com>
2024-10-03 17:38:40 +0200

    #16 Create datav2/dog

---
+++ src/main/java/casp/web/backend/datav2/dog/DogHasHandler.java
@@ -0,0 +1,69 @@
+package casp.web.backend.datav2.dog;
+
+import casp.web.backend.common.BaseDocument;
+import casp.web.backend.common.MemberReference;
+import com.querydsl.core.annotations.QueryEntity;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@QueryEntity
+@Document
+public class DogHasHandler extends BaseDocument {
+
+    @Valid
+    @NotNull
+    @DBRef
+    private MemberReference member;
+
+    @Valid
+    @NotNull
+    @DBRef
+    private Dog dog;
+
+    @NotNull
+    @Valid
+    private Set<Grade> grades = new HashSet<>();
+
+    public MemberReference getMember() {
+        return member;
+    }
+
+    public void setMember(MemberReference member) {
+        this.member = member;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    public Set<Grade> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(Set<Grade> grades) {
+        this.grades = grades;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DogHasHandler that)) return false;
+        if (!super.equals(o)) return false;
+        return Objects.equals(member, that.member) && Objects.equals(dog, that.dog);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(member.getId(), dog.getId());
+    }
+}
---
+++ src/main/java/casp/web/backend/datav2/dog/Grade.java
@@ -0,0 +1,54 @@
+package casp.web.backend.datav2.dog;
+
+import casp.web.backend.common.GradeType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDate;
+
+public class Grade {
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private GradeType type;
+
+    @Positive
+    private long points;
+
+    @NotNull
+    private LocalDate examDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public GradeType getType() {
+        return type;
+    }
+
+    public void setType(final GradeType type) {
+        this.type = type;
+    }
+
+    public long getPoints() {
+        return points;
+    }
+
+    public void setPoints(final long points) {
+        this.points = points;
+    }
+
+    public LocalDate getExamDate() {
+        return examDate;
+    }
+
+    public void setExamDate(final LocalDate examDate) {
+        this.examDate = examDate;
+    }
+}
