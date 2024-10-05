51a8eb293ec851d377901c9ab3835fe6f255f399
Luis Ribeiro <lacribeiro11@gmail.com>
2024-10-05 11:24:03 +0200

    #16 Create datav2/member

---
+++ src/main/java/casp/web/backend/datav2/member/MembershipFee.java
@@ -0,0 +1,53 @@
+package casp.web.backend.datav2.member;
+
+import casp.web.backend.configuration.Payment;
+import casp.web.backend.configuration.PaymentConstraint;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.time.LocalDate;
+
+@PaymentConstraint
+public class MembershipFee implements Payment {
+    private String comment;
+    @PositiveOrZero
+    @Digits(integer = 9, fraction = 2)
+    private double paidPrice;
+    private boolean isPaid;
+    private LocalDate paidDate;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(final String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public double getPaidPrice() {
+        return paidPrice;
+    }
+
+    public void setPaidPrice(final double paidPrice) {
+        this.paidPrice = paidPrice;
+    }
+
+    @Override
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void setPaid(final boolean paid) {
+        isPaid = paid;
+    }
+
+    @Override
+    public LocalDate getPaidDate() {
+        return paidDate;
+    }
+
+    public void setPaidDate(final LocalDate paidDate) {
+        this.paidDate = paidDate;
+    }
+}
