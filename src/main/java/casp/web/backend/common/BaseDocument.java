ccc5853022935e3be6fae58d5308ddfe7b6c08c2
Luis Ribeiro <lacribeiro11@gmail.com>
2024-10-05 11:24:14 +0200

    #16 Move BaseDocument to common

---
+++ src/main/java/casp/web/backend/common/BaseDocument.java
@@ -0,0 +1,105 @@
+package casp.web.backend.common;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public abstract class BaseDocument {
+    @Id
+    protected UUID id = UUID.randomUUID();
+
+    @Version
+    protected long version;
+
+    @CreatedBy
+    protected String createdBy;
+
+    @CreatedDate
+    protected LocalDateTime created;
+
+    @LastModifiedBy
+    protected String modifiedBy;
+
+    @LastModifiedDate
+    protected LocalDateTime modified;
+
+    @NotNull
+    protected EntityStatus entityStatus = EntityStatus.ACTIVE;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
+
+    public EntityStatus getEntityStatus() {
+        return entityStatus;
+    }
+
+    public void setEntityStatus(EntityStatus entityStatus) {
+        this.entityStatus = entityStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseDocument that = (BaseDocument) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
