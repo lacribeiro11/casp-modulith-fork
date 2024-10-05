a86aba4ba01c8a04422979118e9692793b6255a4
Luis Ribeiro <lacribeiro11@gmail.com>
2024-10-05 11:06:10 +0200

    #16 Create DogReference

---
+++ src/main/java/casp/web/backend/common/DogReference.java
@@ -0,0 +1,62 @@
+package casp.web.backend.common;
+
+import com.querydsl.core.annotations.QueryEntity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@QueryEntity
+@Document(collection = "dog")
+public class DogReference {
+    @Id
+    @NotNull
+    private UUID id;
+
+    @NotNull
+    @Indexed
+    private EntityStatus entityStatus;
+
+    @NotBlank
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+
+    public EntityStatus getEntityStatus() {
+        return entityStatus;
+    }
+
+    public void setEntityStatus(final EntityStatus entityStatus) {
+        this.entityStatus = entityStatus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DogReference that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
