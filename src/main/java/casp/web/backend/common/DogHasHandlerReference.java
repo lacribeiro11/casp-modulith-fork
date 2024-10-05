49e9e5223a3c7da97f3123c67c5249c0c01ef234
Luis Ribeiro <lacribeiro11@gmail.com>
2024-10-05 11:14:18 +0200

    #16 Create DogHasHandlerReference

---
+++ src/main/java/casp/web/backend/common/DogHasHandlerReference.java
@@ -0,0 +1,79 @@
+package casp.web.backend.common;
+
+import com.querydsl.core.annotations.QueryEntity;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@QueryEntity
+@Document(collection = "dogHasHandler")
+public class DogHasHandlerReference {
+    @Id
+    @NotNull
+    private UUID id;
+
+    @NotNull
+    @Indexed
+    private EntityStatus entityStatus;
+
+    @Valid
+    @NotNull
+    @DBRef
+    private MemberReference member;
+
+    @Valid
+    @NotNull
+    @DBRef
+    private DogReference dog;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+
+    public EntityStatus getEntityStatus() {
+        return entityStatus;
+    }
+
+    public void setEntityStatus(final EntityStatus entityStatus) {
+        this.entityStatus = entityStatus;
+    }
+
+    public MemberReference getMember() {
+        return member;
+    }
+
+    public void setMember(MemberReference member) {
+        this.member = member;
+    }
+
+    public DogReference getDog() {
+        return dog;
+    }
+
+    public void setDog(DogReference dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DogHasHandlerReference that)) return false;
+        if (!super.equals(o)) return false;
+        return Objects.equals(member, that.member) && Objects.equals(dog, that.dog);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(member.getId(), dog.getId());
+    }
+}
