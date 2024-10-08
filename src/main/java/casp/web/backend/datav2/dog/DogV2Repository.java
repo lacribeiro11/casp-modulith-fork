f87c4d2b2f8547f668d53a648cb9af87689be375
Luis Ribeiro <lacribeiro11@gmail.com>
2024-10-08 22:07:17 +0200

    #16 Remove datav2/dog

--- src/main/java/casp/web/backend/datav2/dog/DogV2Repository.java
+++
@@ -1,9 +0,0 @@
-package casp.web.backend.datav2.dog;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-import java.util.UUID;
-
-public interface DogV2Repository extends MongoRepository<Dog, UUID>, QuerydslPredicateExecutor<Dog> {
-}
