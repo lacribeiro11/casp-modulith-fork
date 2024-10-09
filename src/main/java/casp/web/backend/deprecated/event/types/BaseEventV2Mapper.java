cb0614c4671c60a257b0622c4912fd6c28127b16
Luis Ribeiro <lacribeiro11@gmail.com>
2024-10-09 15:33:24 +0200

    #16 Create BaseEventV2Mapper

---
+++ src/main/java/casp/web/backend/deprecated/event/types/BaseEventV2Mapper.java
@@ -0,0 +1,28 @@
+package casp.web.backend.deprecated.event.types;
+
+import casp.web.backend.data.access.layer.event.types.Course;
+import casp.web.backend.data.access.layer.event.types.Event;
+import casp.web.backend.data.access.layer.event.types.Exam;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+/**
+ * @deprecated It will be removed in #3.
+ */
+@Deprecated(forRemoval = true, since = "0.0.0")
+@Mapper
+public interface BaseEventV2Mapper {
+    BaseEventV2Mapper BASE_EVENT_V2_MAPPER = Mappers.getMapper(BaseEventV2Mapper.class);
+
+    @Mapping(target = "eventType", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    Course toCourse(casp.web.backend.deprecated.event.types.Course course);
+
+    @Mapping(target = "eventType", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    Event toEvent(casp.web.backend.deprecated.event.types.Event event);
+
+    @Mapping(target = "eventType", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    Exam toExam(casp.web.backend.deprecated.event.types.Exam exam);
+}
