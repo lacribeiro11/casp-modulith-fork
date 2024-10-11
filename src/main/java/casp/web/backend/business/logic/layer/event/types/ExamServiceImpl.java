467c3603b29be94630ff608927b0d5081ae09add
Luis Ribeiro <lacribeiro11@gmail.com>
2024-10-11 12:49:02 +0200

    #16 Implement migrateDataToV2 in ExamService

--- src/main/java/casp/web/backend/business/logic/layer/event/types/ExamServiceImpl.java
+++ src/main/java/casp/web/backend/business/logic/layer/event/types/ExamServiceImpl.java
@@ -2,21 +2,82 @@ package casp.web.backend.business.logic.layer.event.types;
 
 import casp.web.backend.business.logic.layer.event.calendar.CalendarService;
 import casp.web.backend.business.logic.layer.event.participants.ExamParticipantService;
+import casp.web.backend.common.MemberReference;
+import casp.web.backend.data.access.layer.event.types.DogHasHandlerReferenceRepository;
+import casp.web.backend.data.access.layer.event.types.ExamV2Repository;
+import casp.web.backend.data.access.layer.event.types.MemberReferenceRepository;
 import casp.web.backend.data.access.layer.member.MemberRepository;
+import casp.web.backend.deprecated.event.calendar.CalendarRepository;
+import casp.web.backend.deprecated.event.participants.BaseParticipantRepository;
 import casp.web.backend.deprecated.event.participants.ExamParticipant;
 import casp.web.backend.deprecated.event.types.BaseEventRepository;
 import casp.web.backend.deprecated.event.types.Exam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static casp.web.backend.deprecated.event.participants.BaseParticipantV2Mapper.BASE_PARTICIPANT_V2_MAPPER;
+import static casp.web.backend.deprecated.event.types.BaseEventV2Mapper.BASE_EVENT_V2_MAPPER;
+
 @Service
 class ExamServiceImpl extends BaseEventServiceImpl<Exam, ExamParticipant> implements ExamService {
+
+    private final ExamV2Repository examV2Repository;
+    private final BaseParticipantRepository baseParticipantRepository;
+    private final DogHasHandlerReferenceRepository dogHasHandlerReferenceRepository;
 
     @Autowired
     ExamServiceImpl(final CalendarService calendarService,
                     final ExamParticipantService participantService,
                     final BaseEventRepository eventRepository,
-                    final MemberRepository memberRepository) {
-        super(calendarService, participantService, eventRepository, memberRepository, Exam.EVENT_TYPE);
+                    final MemberRepository memberRepository,
+                    final MemberReferenceRepository memberReferenceRepository,
+                    final CalendarRepository calendarRepository,
+                    final ExamV2Repository examV2Repository,
+                    final BaseParticipantRepository baseParticipantRepository,
+                    final DogHasHandlerReferenceRepository dogHasHandlerReferenceRepository) {
+        super(calendarService, participantService, eventRepository, memberRepository, Exam.EVENT_TYPE, memberReferenceRepository, calendarRepository);
+        this.examV2Repository = examV2Repository;
+        this.baseParticipantRepository = baseParticipantRepository;
+        this.dogHasHandlerReferenceRepository = dogHasHandlerReferenceRepository;
+    }
+
+    @Override
+    public void migrateDataToV2() {
+        examV2Repository.deleteAll();
+
+        eventRepository.findAllByEventType(Exam.EVENT_TYPE).forEach(e ->
+                findMemberReference(e.getMemberId()).ifPresent(m -> mapAndSaveExamV2((Exam) e, m)));
+    }
+
+    private void mapAndSaveExamV2(final Exam exam, final MemberReference examMember) {
+        var examV2 = BASE_EVENT_V2_MAPPER.toExam(exam);
+
+        examV2.setMember(examMember);
+        examV2.setParticipants(mapParticipants(exam));
+
+        mapToCalendarEntries(exam.getId(), examV2);
+        mapToBaseEventOptionV2(exam).ifPresent(examV2::setBaseEventOption);
+
+        examV2Repository.save(examV2);
+    }
+
+    private Set<casp.web.backend.data.access.layer.event.participants.ExamParticipant> mapParticipants(final Exam exam) {
+        return baseParticipantRepository.findAllByBaseEventIdAndParticipantType(exam.getId(), ExamParticipant.PARTICIPANT_TYPE)
+                .stream()
+                .flatMap(p -> mapParticipant((ExamParticipant) p).stream())
+                .collect(Collectors.toSet());
+    }
+
+    private Optional<casp.web.backend.data.access.layer.event.participants.ExamParticipant> mapParticipant(final ExamParticipant participant) {
+        return dogHasHandlerReferenceRepository.findById(participant.getMemberOrHandlerId())
+                .map(dh -> {
+                    var examParticipantV2 = BASE_PARTICIPANT_V2_MAPPER.toExamParticipant(participant);
+                    examParticipantV2.setDogHasHandler(dh);
+                    return examParticipantV2;
+                });
     }
 }
--- src/test/java/casp/web/backend/business/logic/layer/event/types/ExamServiceImplTest.java
+++ src/test/java/casp/web/backend/business/logic/layer/event/types/ExamServiceImplTest.java
@@ -4,8 +4,18 @@ package casp.web.backend.business.logic.layer.event.types;
 import casp.web.backend.TestFixture;
 import casp.web.backend.business.logic.layer.event.calendar.CalendarService;
 import casp.web.backend.business.logic.layer.event.participants.ExamParticipantService;
+import casp.web.backend.common.BaseEventOptionType;
+import casp.web.backend.common.DogHasHandlerReference;
 import casp.web.backend.common.EntityStatus;
+import casp.web.backend.common.MemberReference;
+import casp.web.backend.data.access.layer.event.types.DogHasHandlerReferenceRepository;
+import casp.web.backend.data.access.layer.event.types.ExamV2Repository;
+import casp.web.backend.data.access.layer.event.types.MemberReferenceRepository;
 import casp.web.backend.data.access.layer.member.MemberRepository;
+import casp.web.backend.deprecated.event.calendar.Calendar;
+import casp.web.backend.deprecated.event.calendar.CalendarRepository;
+import casp.web.backend.deprecated.event.participants.BaseParticipantRepository;
+import casp.web.backend.deprecated.event.participants.ExamParticipant;
 import casp.web.backend.deprecated.event.types.BaseEvent;
 import casp.web.backend.deprecated.event.types.BaseEventRepository;
 import casp.web.backend.deprecated.event.types.Exam;
@@ -13,11 +23,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -27,10 +41,15 @@ import java.util.Set;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,7 +63,6 @@ class ExamServiceImplTest {
     private BaseEventRepository eventRepository;
     @Mock
     private MemberRepository memberRepository;
-
     @InjectMocks
     private ExamServiceImpl examService;
 
@@ -64,28 +82,6 @@ class ExamServiceImplTest {
 
         assertSame(exam, examService.save(exam));
         verify(exam).setMember(member);
-    }
-
-    @Nested
-    class DeleteById {
-        @Test
-        void exist() {
-            when(eventRepository.findByIdAndEntityStatus(exam.getId(), EntityStatus.ACTIVE)).thenReturn(Optional.of(exam));
-
-            examService.deleteById(exam.getId());
-
-            verify(participantService).deleteParticipantsByBaseEventId(exam.getId());
-            verify(calendarService).deleteCalendarEntriesByBaseEventId(exam.getId());
-            assertSame(EntityStatus.DELETED, exam.getEntityStatus());
-        }
-
-        @Test
-        void doesNotExist() {
-            var id = UUID.randomUUID();
-            when(eventRepository.findByIdAndEntityStatus(id, EntityStatus.ACTIVE)).thenReturn(Optional.empty());
-
-            assertThrows(NoSuchElementException.class, () -> examService.deleteById(id));
-        }
     }
 
     @Test
@@ -136,6 +132,28 @@ class ExamServiceImplTest {
     }
 
     @Nested
+    class DeleteById {
+        @Test
+        void exist() {
+            when(eventRepository.findByIdAndEntityStatus(exam.getId(), EntityStatus.ACTIVE)).thenReturn(Optional.of(exam));
+
+            examService.deleteById(exam.getId());
+
+            verify(participantService).deleteParticipantsByBaseEventId(exam.getId());
+            verify(calendarService).deleteCalendarEntriesByBaseEventId(exam.getId());
+            assertSame(EntityStatus.DELETED, exam.getEntityStatus());
+        }
+
+        @Test
+        void doesNotExist() {
+            var id = UUID.randomUUID();
+            when(eventRepository.findByIdAndEntityStatus(id, EntityStatus.ACTIVE)).thenReturn(Optional.empty());
+
+            assertThrows(NoSuchElementException.class, () -> examService.deleteById(id));
+        }
+    }
+
+    @Nested
     class GetBaseEventById {
         @Test
         void eventExist() {
@@ -153,6 +171,119 @@ class ExamServiceImplTest {
             when(eventRepository.findByIdAndEntityStatus(id, EntityStatus.ACTIVE)).thenReturn(Optional.empty());
 
             assertThrows(NoSuchElementException.class, () -> examService.getOneById(id));
+        }
+    }
+
+    @Nested
+    class MigrateDataToV2 {
+        private static final Sort SORT = Sort.by("eventFrom").ascending().and(Sort.by("eventTo").ascending());
+        @Mock
+        private ExamV2Repository examV2Repository;
+        @Mock
+        private MemberReferenceRepository memberReferenceRepository;
+        @Mock
+        private CalendarRepository calendarRepository;
+        @Mock
+        private BaseParticipantRepository participantRepository;
+        @Mock
+        private DogHasHandlerReferenceRepository dogHasHandlerReferenceRepository;
+        @Captor
+        private ArgumentCaptor<casp.web.backend.data.access.layer.event.types.Exam> examV2Captor;
+        @Mock
+        private MemberReference examMember;
+
+        private casp.web.backend.data.access.layer.event.types.Exam examV2;
+        private Calendar calendar;
+
+
+        @BeforeEach
+        void setUp() {
+            when(eventRepository.findAllByEventType(Exam.EVENT_TYPE)).thenReturn(Set.of(exam));
+        }
+
+        @Test
+        void deleteAll() {
+            examService.migrateDataToV2();
+
+            verify(examV2Repository).deleteAll();
+        }
+
+        @Test
+        void examMemberNotFound() {
+            when(memberReferenceRepository.findById(exam.getMemberId())).thenReturn(Optional.empty());
+
+            examService.migrateDataToV2();
+
+            verifyNoInteractions(calendarRepository, participantRepository, dogHasHandlerReferenceRepository);
+            verify(examV2Repository).deleteAll();
+            verify(examV2Repository, times(0)).save(any(casp.web.backend.data.access.layer.event.types.Exam.class));
+        }
+
+        @Test
+        void participants() {
+            findExamMemberAndCalendar();
+            var participantDogHasHandler = mock(DogHasHandlerReference.class, Answers.RETURNS_DEEP_STUBS);
+            when(participantDogHasHandler.getEntityStatus()).thenReturn(EntityStatus.ACTIVE);
+            when(participantDogHasHandler.getMember().getEntityStatus()).thenReturn(EntityStatus.ACTIVE);
+            when(participantDogHasHandler.getDog().getEntityStatus()).thenReturn(EntityStatus.ACTIVE);
+            var examParticipant = TestFixture.createExamParticipant();
+            when(participantRepository.findAllByBaseEventIdAndParticipantType(exam.getId(), ExamParticipant.PARTICIPANT_TYPE)).thenReturn(Set.of(examParticipant));
+            when(dogHasHandlerReferenceRepository.findById(examParticipant.getMemberOrHandlerId())).thenReturn(Optional.of(participantDogHasHandler));
+
+            examService.migrateDataToV2();
+
+            assertExamV2();
+            assertThat(examV2.getParticipants()).singleElement().satisfies(s -> assertSame(participantDogHasHandler, s.getDogHasHandler()));
+        }
+
+        @Test
+        void dogHasHandlerForParticipantNotFound() {
+            findExamMemberAndCalendar();
+            var examParticipant = TestFixture.createExamParticipant();
+            when(participantRepository.findAllByBaseEventIdAndParticipantType(exam.getId(), ExamParticipant.PARTICIPANT_TYPE)).thenReturn(Set.of(examParticipant));
+            when(dogHasHandlerReferenceRepository.findById(examParticipant.getMemberOrHandlerId())).thenReturn(Optional.empty());
+
+            examService.migrateDataToV2();
+
+            assertExamV2();
+            assertThat(examV2.getParticipants()).isEmpty();
+        }
+
+
+        @Test
+        void daily() {
+            findExamMemberAndCalendar();
+            exam.setDailyOption(TestFixture.createDailyEventOption());
+
+            examService.migrateDataToV2();
+
+            assertExamV2();
+            assertSame(BaseEventOptionType.DAILY, examV2.getBaseEventOption().getOptionType());
+        }
+
+        @Test
+        void weekly() {
+            findExamMemberAndCalendar();
+            exam.setWeeklyOption(TestFixture.createWeeklyEventOption());
+
+            examService.migrateDataToV2();
+
+            assertExamV2();
+            assertSame(BaseEventOptionType.WEEKLY, examV2.getBaseEventOption().getOptionType());
+        }
+
+        private void findExamMemberAndCalendar() {
+            when(memberReferenceRepository.findById(exam.getMemberId())).thenReturn(Optional.of(examMember));
+            calendar = TestFixture.createCalendarEntry();
+            when(calendarRepository.findAllByBaseEventId(exam.getId(), SORT)).thenReturn(List.of(calendar));
+        }
+
+        private void assertExamV2() {
+            verify(examV2Repository).save(examV2Captor.capture());
+            examV2 = examV2Captor.getValue();
+            assertEquals(exam.getId(), examV2.getId());
+            assertSame(examMember, examV2.getMember());
+            assertThat(examV2.getCalendarEntries()).singleElement().satisfies(ce -> assertEquals(calendar.getId(), ce.getId()));
         }
     }
 }
